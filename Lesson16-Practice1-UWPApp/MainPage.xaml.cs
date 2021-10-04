cf1793b8f4320d4704d9df6a1879f43e34a12fe4
Владислав Куманов <edu.kumanov@outlook.com>
2021-10-04 21:44:59 +0300

    Add project files.

---
+++ Lesson16-Practice1-UWPApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.SpeechSynthesis;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lesson16_Practice1_UWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        string speakIt = "Hello, World!";
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            // checking if the text provided in the textbox is null or whitespace
+        if (!string.IsNullOrWhiteSpace(SpeakText.Text))
+                speakIt = SpeakText.Text;
+            else
+                speakIt = "Please enter a valid string!";
+
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(speakIt);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+
+        }
+    }
+}
